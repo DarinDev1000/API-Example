a7f168e01923264ac94d2e58c4076a94b82360fa
Darin <darindev1000@gmail.com>
2019-05-17 16:04:56 -0700

    Basic API Example with thingiverse search

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import Axios from 'axios';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+  // Default Variables
+  searchName = 'cat';
+  baseUrl = 'https://api.thingiverse.com/search/';
+  urlAccessToken = '?access_token=a34aca11d88f36b8bdd1793cfeafd2b0';
+  responseItems = [];
+
+  constructor() { }
+
+  // This is run at startup
+  ngOnInit() {
+    this.runSearch();
+  }
+
+  // This is run on the search button
+  async runSearch() {
+    const url = `${this.baseUrl}${this.searchName}${this.urlAccessToken}`;
+    const response = await Axios.get(url);
+    console.log(response.data);
+    this.responseItems = response.data;
+  }
+}
